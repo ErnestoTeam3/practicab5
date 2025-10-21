22202a62ba191542a3f819d90aace523c1caed5c
ernesto <ernestoalonsocorzoteam3@gmail.com>
2025-10-21 11:34:27 -0600

    .

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 export const runtime = "nodejs";
 
 // Variables de entorno
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 // Validación rápida para build/runtime
